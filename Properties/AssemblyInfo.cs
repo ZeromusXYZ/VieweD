5938b60e8a922de5b524731529ef627d0a74f8e4
ZeromusXYZ <zeromus.xyz@gmail.com>
2023-01-26 12:10:14 +0100

    -fix: Number input parsing (#2)
    Fixes issue #1 where all number were parsed as being hex, even without the prefix/suffix for them.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZeromusXYZ")]
 [assembly: AssemblyProduct("VieweD")]
-[assembly: AssemblyCopyright("Copyright © 2020-2022, No Rights Reserved")]
+[assembly: AssemblyCopyright("Copyright © 2020-2023, No Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.2")]
-[assembly: AssemblyFileVersion("0.9.0.2")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
 [assembly: Packer]
