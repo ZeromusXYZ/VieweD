719450f7b68c7e3d2b2ac8bff891a262ba93c8ad
ZeromusXYZ <zeromus.xyz@gmail.com>
2024-05-18 21:33:05 +0200

    -wip: cleaning
    - Added some new checks to decompression to avoid infinite loops
    - Removed old commented code

--- VieweD/data/aa/engine/AaPCapInputReader.cs
+++ VieweD/data/aa/engine/AaPCapInputReader.cs
@@ -60,6 +60,8 @@ public class AaPCapInputReader : AaBaseInputReader
                 ParentProject.TimeStampFormat = "HH:mm:ss.fff";
             MinTime = DateTime.MaxValue;
             MaxTime = DateTime.MinValue;
+            // XorKey = 0x6D783F3C;
+            // AesKey = new byte[] { 0x3C, 0x3F, 0x78, 0x6D, 0x6C, 0x20, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6F, 0x6E, 0x3D, 0x22, 0x31 };
 
             return true;
         }
@@ -97,135 +99,6 @@ public class AaPCapInputReader : AaBaseInputReader
             ReaderDevice.Open();
             ReaderDevice.Capture(); // 50 for testing // Reads all the data
 
-            /*
-
-            while (Reader.BaseStream.Position < Reader.BaseStream.Length)
-            {
-                // update position
-                if ((packetCounter % 250) == 0)
-                    ViewedProjectTab.OnInputProgressUpdate(this, (int)Reader.BaseStream.Position, (int)Reader.BaseStream.Length);
-
-                var fileCmd = Reader.ReadByte();
-                var sourceStreamId = (byte)(Reader.ReadUInt32() % 0x100);
-                var offsetTime = Reader.ReadSingle();
-
-
-                switch (fileCmd)
-                {
-                    case 1: // Connect command
-                        var ip4 = Reader.ReadBytes(4);
-                        var currentIpv4 = string.Join('.', ip4);
-                        var currentPort = Reader.ReadUInt16();
-                        ipMapping.TryAdd(sourceStreamId, currentIpv4);
-                        localStreamToPortMapping.TryAdd(sourceStreamId, currentPort);
-                        ParentProject.RegisterPort(currentPort, "Port " + currentPort, "?");
-                        // currentStreamInfo = ParentProject.GetExpectedStreamIdByPort(currentPort, sourceStreamId);
-                        break;
-                    case 2: // out
-                    case 3: // in
-                        var dataSize = Reader.ReadInt32();
-                        var rawData = Reader.ReadBytes(dataSize);
-
-
-                        var data = new BasePacketData(ParentProject);
-                        if (fileCmd == 2)
-                            data.PacketDataDirection = PacketDataDirection.Outgoing;
-                        if (fileCmd == 3)
-                            data.PacketDataDirection = PacketDataDirection.Incoming;
-
-                        // Get Port for this stream
-                        if (localStreamToPortMapping.TryGetValue(sourceStreamId, out var localPort))
-                            data.SourcePort = localPort;
-
-                        var streamInfoFromPort = ParentProject.GetExpectedStreamIdByPort(data.SourcePort, 0);
-
-                        if (ipMapping.TryGetValue(sourceStreamId, out var ip))
-                            data.SourceIp = ip;
-                        data.TimeStamp = DateTime.MinValue.AddMilliseconds(offsetTime * 1000.0);
-
-                        // grab min and max timestamp used
-                        if (data.TimeStamp > maxTime)
-                            maxTime = data.TimeStamp;
-                        if (data.TimeStamp < minTime)
-                            minTime = data.TimeStamp;
-
-                        data.ByteData.AddRange(rawData);
-                        data.SyncId = dummySync;
-                        dummySync++;
-
-                        // Get Base info
-                        data.Cursor = 0;
-                        byte dirByte3;
-                        if (streamInfoFromPort.Item3 != "?")
-                        {
-                            data.PacketDataSize = data.GetUInt16AtPos(data.Cursor);
-                            data.PacketId = 0xFFFF;
-                            dirByte3 = data.GetByteAtPos(data.Cursor);
-                            data.CompressionLevel = data.GetByteAtPos(data.Cursor);
-                        }
-                        else
-                        {
-                            dirByte3 = 0;
-                            data.PacketDataSize = data.ByteData.Count;
-                            data.PacketId = 0x0000;
-                            data.CompressionLevel = 0;
-                        }
-
-                        switch (data.CompressionLevel)
-                        {
-                            case 0: // Simple packet
-                                data.PacketId = dirByte3;
-                                break;
-                            case 1: // Packet with Hash
-                                _ = data.GetUInt16AtPos(data.Cursor); // Hash L1
-                                data.PacketId = data.GetUInt16AtPos(data.Cursor);
-                                //pd.PacketLevel = 0;
-                                break;
-                            case 2: // Extra Packet
-                                data.PacketId = data.GetUInt16AtPos(data.Cursor);
-                                break;
-                            case 3: // Do Decompress L3
-                                if (DecompressL3Data(data) == false)
-                                {
-                                    data.MarkedAsInvalid = true;
-                                    //return false;
-                                }
-                                break;
-                            case 4: // Do Decompress L4
-                                if (DecompressL4Data(data) == false)
-                                {
-                                    data.MarkedAsInvalid = true;
-                                    //return false;
-                                }
-                                break;
-                            case 5: // Do decode L5
-                                if (DecodeL5Data(data) == false)
-                                {
-                                    data.MarkedAsInvalid = true;
-                                    // return false;
-                                }
-                                break;
-                        }
-
-                        if (data.UnParseSubPacketCount > 0)
-                            ParentProject.RequiresSubPacketCreation = true;
-
-                        if (CompileData(data))
-                            ParentProject.LoadedPacketList.Add(data);
-
-                        break;
-                    case 4: // Disconnect
-                        // Do nothing
-                        break;
-                    default:
-                        MessageBox.Show(string.Format(Resources.ReadAllDataUnknownCommand, fileCmd, Reader.BaseStream.Position), Resources.ReadAllDataFileFormatErrorTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return -1;
-                }
-
-            }
-
-            */
-
             // shorten the timestamp if the total capture is less than 1 hour
             var dMin = minTime - DateTime.MinValue;
             var dMax = maxTime - DateTime.MinValue;
@@ -233,7 +106,7 @@ public class AaPCapInputReader : AaBaseInputReader
                 ParentProject.TimeStampFormat = "mm:ss.fff";
 
             // end progress update
-            // ViewedProjectTab.OnInputProgressUpdate(this, (int)Reader.BaseStream.Length, (int)Reader.BaseStream.Length);
+            ViewedProjectTab.OnInputProgressUpdate(this, 100, 100);
         }
         catch (Exception ex)
         {
@@ -275,6 +148,9 @@ public class AaPCapInputReader : AaBaseInputReader
         if (tcp == null)
             return;
 
+        if (tcp.PayloadData.Length <= 0)
+            return;
+
         var data = new BasePacketData(ParentProject);
         data.SourceIp = ip4Packet.SourceAddress.ToString();
         data.DestinationIp = ip4Packet.DestinationAddress.ToString();
@@ -303,14 +179,6 @@ public class AaPCapInputReader : AaBaseInputReader
             data.DestinationPort = dPort;
             data.PacketDataDirection = PacketDataDirection.Unknown;
         }
-
-        //if (CompileData(data))
-        //    ParentProject.LoadedPacketList.Add(data);
-
-        var localStreamToPortMapping = new Dictionary<byte, ushort>();
-
-        // update position
-        // ViewedProjectTab.OnInputProgressUpdate(this, (int)Reader.BaseStream.Position, (int)Reader.BaseStream.Length);
 
         // Get Port for this stream
         var streamInfoFromPort = ParentProject.GetExpectedStreamIdByPort(data.SourcePort, 0);
@@ -349,7 +217,7 @@ public class AaPCapInputReader : AaBaseInputReader
                 data.PacketId = dirByte3;
                 break;
             case 1: // Packet with Hash
-                _ = data.GetUInt16AtPos(data.Cursor); // Hash L1
+                var l1Hash = data.GetUInt16AtPos(data.Cursor); // Hash L1
                 data.PacketId = data.GetUInt16AtPos(data.Cursor);
                 //pd.PacketLevel = 0;
                 break;
@@ -384,6 +252,9 @@ public class AaPCapInputReader : AaBaseInputReader
 
         if (CompileData(data))
             ParentProject.LoadedPacketList.Add(data);
+
+        // update position
+        ViewedProjectTab.OnInputProgressUpdate(this, data.SyncId % 0x0100, 0x0100);
     }
 
     private void ReaderDeviceOnPacketStopped(object sender, CaptureStoppedEventStatus status)
